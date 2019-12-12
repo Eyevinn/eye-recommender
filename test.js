41f6c359b7916dc462e440f9ce8977f47693954c
Erik Hoffman <erik.johansson87@gmail.com>
2019-12-12 10:15:28 +0100

    updated the example file to generate more random data

--- test.js
+++ test.js
@@ -22,13 +22,42 @@ const Movies = [
   "Die Hard 3"
 ];
 
-let counter = 50;
 const go = async () => {
   let randomPerson =
     People[Math.floor(Math.random() * (People.length - 0) + 0)];
   let randomMovie = Movies[Math.floor(Math.random() * (Movies.length - 0) + 0)];
-  console.log(`${randomPerson} likes ${randomMovie}`);
-  await eyeRecommender.input.like(randomPerson, randomMovie);
+
+  const randomNumber = Math.floor(Math.random() * 100);
+  const inputType =
+    randomNumber <= 25
+      ? "like"
+      : randomNumber > 25 && randomNumber <= 50
+      ? "dislike"
+      : randomNumber > 50 && randomNumber <= 75
+      ? "unlike"
+      : randomNumber > 75
+      ? "undislike"
+      : "";
+  switch (inputType) {
+    case "like":
+      console.log(`${randomPerson} likes ${randomMovie}`);
+      await eyeRecommender.input.like(randomPerson, randomMovie);
+      break;
+    case "dislike":
+      console.log(`${randomPerson} dislikes ${randomMovie}`);
+      await eyeRecommender.input.dislike(randomPerson, randomMovie);
+      break;
+    case "unlike":
+      console.log(`${randomPerson} unlikes ${randomMovie}`);
+      await eyeRecommender.input.unlike(randomPerson, randomMovie);
+      break;
+    case "undislike":
+      console.log(`${randomPerson} undislikes ${randomMovie}`);
+      await eyeRecommender.input.undislike(randomPerson, randomMovie);
+      break;
+    default:
+      break;
+  }
 
   let anotherPersion =
     People[Math.floor(Math.random() * (People.length - 0) + 0)];
@@ -37,10 +66,16 @@ const go = async () => {
   );
   console.log(`Recommendations for ${anotherPersion}`, recommendations);
 
-  counter--;
-  if (counter > 0) {
-    setTimeout(go, 1000);
-  }
+  setTimeout(go, 1000);
+};
+
+const stats = async () => {
+  console.log(
+    "Best rated",
+    await eyeRecommender.statistics.bestRatedWithScores()
+  );
+  console.log("Most liked", await eyeRecommender.statistics.mostLiked());
+  console.log("Most disliked", await eyeRecommender.statistics.mostDisliked());
 };
 
 (async () => {
