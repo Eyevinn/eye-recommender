d6caeecf8390dd4dbe8cab1c104406d2dd537c69
Erik Hoffman <erik.johansson87@gmail.com>
2019-12-12 09:37:02 +0100

    documentation

--- src/handlers/StatisticsHandler.js
+++ src/handlers/StatisticsHandler.js
@@ -6,6 +6,12 @@ class StatisticsHandler extends BaseRedisHandler {
     super({ redisClient });
   }
 
+  /**
+   * Get the list of recommendations for named user
+   *
+   * @param {string} userId
+   * @param {number} numberOfRecs
+   */
   async recommendFor(userId, numberOfRecs = 10) {
     return await this.redisClient.zrevrangeAsync(
       keyBuilder.recommendedZSet(userId),
@@ -14,6 +20,9 @@ class StatisticsHandler extends BaseRedisHandler {
     );
   }
 
+  /**
+   * Get the items with the best ratings in general
+   */
   async bestRated() {
     return await this.redisClient.zrevrangeAsync(
       keyBuilder.scoreboardZSet(),
@@ -22,6 +31,9 @@ class StatisticsHandler extends BaseRedisHandler {
     );
   }
 
+  /**
+   * Get the items with the worst ratings in general
+   */
   async worstRated() {
     return await this.redisClient.zrangeAsync(
       keyBuilder.scoreboardZSet(),
@@ -30,6 +42,12 @@ class StatisticsHandler extends BaseRedisHandler {
     );
   }
 
+  /**
+   * Get the items with the best ratings in general
+   * includes scores in the result
+   *
+   * @param {number} numOfRatings
+   */
   async bestRatedWithScores(numOfRatings = 10) {
     return await this.redisClient.zrevrangeAsync(
       keyBuilder.scoreboardZSet(),
@@ -39,10 +57,16 @@ class StatisticsHandler extends BaseRedisHandler {
     );
   }
 
+  /**
+   * Get the most liked assets
+   */
   async mostLiked() {
     return await this.redisClient.zrevrangeAsync(keyBuilder.mostLiked(), 0, -1);
   }
 
+  /**
+   * Get the most disliked assets
+   */
   async mostDisliked() {
     return await this.redisClient.zrevrangeAsync(
       keyBuilder.mostDisliked(),
@@ -51,6 +75,11 @@ class StatisticsHandler extends BaseRedisHandler {
     );
   }
 
+  /**
+   * Get the most similar users for a given user
+   *
+   * @param {string} userId
+   */
   async mostSimilarUsers(userId) {
     return await this.redisClient.zrevrangeAsync(
       keyBuilder.similarityZSet(userId),
@@ -59,6 +88,11 @@ class StatisticsHandler extends BaseRedisHandler {
     );
   }
 
+  /**
+   * Get the least similar users for a given user
+   *
+   * @param {string} userId
+   */
   async leastSimilarUsers(userId) {
     return await this.redisClient.zrangeAsync(
       keyBuilder.similarityZSet(userId),
@@ -67,40 +101,68 @@ class StatisticsHandler extends BaseRedisHandler {
     );
   }
 
+  /**
+   * Get a list of users who liked a given asset
+   * @param {string} itemId
+   */
   async likedBy(itemId) {
     return await this.redisClient.smembersAsync(
       keyBuilder.itemLikedBySet(itemId)
     );
   }
 
+  /**
+   * Get the amount of users who liked a given asset
+   * @param {string} itemId
+   */
   async likedCount(itemId) {
     return await this.redisClient.scardAsync(keyBuilder.itemLikedBySet(itemId));
   }
 
+  /**
+   * Get a list of users who disliked a given asset
+   * @param {string} itemId
+   */
   async dislikedBy(itemId) {
     return await this.redisClient.smembersAsync(
       keyBuilder.itemDislikedBySet(itemId)
     );
   }
 
+  /**
+   * Get the amount of users who disliked a given asset
+   * @param {string} itemId
+   */
   async dislikedCount(itemId) {
     return await this.redisClient.scardAsync(
       keyBuilder.itemDislikedBySet(itemId)
     );
   }
 
+  /**
+   * Get a list of items that the given user has liked
+   * @param {string} userId
+   */
   async allLikedFor(userId) {
     return await this.redisClient.smembersAsync(
       keyBuilder.userLikedSet(userId)
     );
   }
 
+  /**
+   * Get a list of items that the given user has disliked
+   * @param {string} userId
+   */
   async allDislikedFor(userId) {
     return await this.redisClient.smembersAsync(
       keyBuilder.userDislikedSet(userId)
     );
   }
 
+  /**
+   * Get a list of items that the given user has rated
+   * @param {string} userId
+   */
   async allWatchedFor(userId) {
     return await this.redisClient.sunionAsync(
       keyBuilder.userLikedSet(userId),
